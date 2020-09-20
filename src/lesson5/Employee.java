081bd4b2fc390323c65d63986e747d02973ba248
Nikolay <neparnak@gmail.com>
2020-09-20 17:04:35 +0400

    lesson5

--- src/lesson5/Employee.java
+++ src/lesson5/Employee.java
@@ -1,14 +1,14 @@
 package lesson5;
 
 public class Employee {
-    String name;
-    String position;
-    String mail;
-    int phoneNum;
-    int salary;
-    int age;
+    private String name;
+    private String position;
+    private String mail;
+    private String phoneNum;
+    private int salary;
+    private int age;
 
-    Employee (String name, String position, String mail, int phoneNum, int salary, int age){
+    Employee (String name, String position, String mail, String phoneNum, int salary, int age){
         this.name = name;
         this.position = position;
         this.mail = mail;
@@ -16,7 +16,12 @@ public class Employee {
         this.salary = salary;
         this.age = age;
     }
-    public void getInfo (){
+
+    public int getAge(){
+        return age;
+    }
+
+    public void printInfo (){
         System.out.println();
         System.out.println("Имя: " + name);
         System.out.println("Дожность: " + position);
